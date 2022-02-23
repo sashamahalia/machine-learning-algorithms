1af0898737f65b8030c93e50e37ddd06069604f6
Sasha <sasha.mahalia@beatdapp.com>
2022-02-23 15:47:42 -0800

    Refactor project, load data for nearest neighbor algorythym

--- linear-regression/linearregression.py
+++ linear-regression/linearregression.py
@@ -10,7 +10,7 @@ import pickle
 from matplotlib import style
 
 
-data = pd.read_csv("student-mat.csv", sep=";")
+data = pd.read_csv("./linear-regression/student-mat.csv", sep=";")
 
 data = data[["G1", "G2", "G3", "studytime", "absences"]]
 
@@ -32,10 +32,10 @@ x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y
 #
 #     if acc > best:
 #         best = acc
-#         with open("studentmodel.pickle", "wb") as f:
+#         with open("./linear-regression/studentmodel.pickle", "wb") as f:
 #             pickle.dump(linear, f)
 
-pickle_in = open("studentmodel.pickle", "rb")
+pickle_in = open("./linear-regression/studentmodel.pickle", "rb")
 linear = pickle.load(pickle_in)
 
 predictions = linear.predict(x_test)
@@ -43,4 +43,9 @@ predictions = linear.predict(x_test)
 for x in range(len(predictions)):
     print(predictions[x], x_test[x], y_test[x])
 
+p = "absences"
 style.use("ggplot")
+pyplot.scatter(data[p], data["G3"])
+pyplot.xlabel(p)
+pyplot.ylabel("Final Grade")
+pyplot.show()
