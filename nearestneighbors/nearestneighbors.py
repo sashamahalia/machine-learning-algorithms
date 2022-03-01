657d68ec4ed7128fffa57a6dcd53e7eb551c0823
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2022-03-01 14:03:42 -0800

    save nearest neighbour indexes

--- nearestneighbors/nearestneighbors.py
+++ nearestneighbors/nearestneighbors.py
@@ -35,3 +35,5 @@ names = ["unacc", "acc", "good", "vgood"]
 
 for x in range(len(predicted)):
     print("Predicted: ", names[predicted[x]], "Data: ", x_test[x], "Actual: ", names[y_test[x]])
+    n = model.kneighbors([x_test[x]], 9, True)
+    print("N: ", n)
