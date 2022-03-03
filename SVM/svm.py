cdfa9e9dda212736724bb2ef7b91b7f8b47ac904
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2022-03-03 15:17:25 -0800

    setup term frequency and inverse document frequency transformation

--- SVM/svm.py
+++ SVM/svm.py
@@ -3,7 +3,9 @@ from sklearn import datasets, svm, metrics, preprocessing
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.linear_model import SGDClassifier
+
 
 # cancer = datasets.load_breast_cancer()
 #
@@ -13,37 +15,39 @@ from sklearn.neighbors import KNeighborsClassifier
 data = pd.read_csv("../news.csv", sep=",")
 data = data[["title", "text", "label"]]
 predict = "label"
-title_list = data['title'].values.tolist()
 text_list = data['text'].values.tolist()
 
 
 # title = sklearn.feature_extraction.text.CountVectorizer(data["title"])
 #
 vectorizer = CountVectorizer()
-vectorizer2 = CountVectorizer()
 
-title_vectorized = vectorizer.fit_transform(title_list)
-text_vectorized = vectorizer2.fit_transform(text_list)
+text_vectorized = vectorizer.fit_transform(text_list)
 
-X = list(zip(title_vectorized, text_vectorized))
+# Term frequency transformer with idf (Inverse Document Frequency) enabled
+tfidf_transformer = TfidfTransformer(use_idf=True)
+X_train_tfidf = tfidf_transformer.fit_transform(text_vectorized)
+print("X data shape: ", X_train_tfidf.shape)
 
 le = preprocessing.LabelEncoder()
 label = le.fit_transform(list(data["label"]))
 y = list(label)
 
-print(vectorizer2.vocabulary_.get(u'compared'))
+print(f"Occurrences of search term: ", vectorizer.vocabulary_.get(u'compared'))
+
+
 #
 # X = list(zip(title, text))
 # y = list(label)
 #
-#
-# x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.2)
-#
-# clf = svm.SVC(kernel="linear", C=2)
-# clf.fit(x_train, y_train)
-#
-# y_predict = clf.predict(x_test)
-#
-# acc = metrics.accuracy_score(y_test, y_predict)
-#
-# print(acc)
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X_train_tfidf, y, test_size=0.2)
+
+clf = svm.SVC(kernel="linear", C=2)
+clf.fit(x_train, y_train)
+
+y_predict = clf.predict(x_test)
+
+acc = metrics.accuracy_score(y_test, y_predict)
+
+print(acc)
