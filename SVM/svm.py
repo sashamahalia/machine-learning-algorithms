900c30e5d944961832285913d0d8680f52df183b
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2022-03-03 14:28:57 -0800

    Vectorize fake news data in svm algo

--- SVM/svm.py
+++ SVM/svm.py
@@ -2,6 +2,7 @@ import sklearn
 from sklearn import datasets, svm, metrics, preprocessing
 import pandas as pd
 import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.neighbors import KNeighborsClassifier
 
 # cancer = datasets.load_breast_cancer()
@@ -10,27 +11,49 @@ from sklearn.neighbors import KNeighborsClassifier
 # y = cancer.target
 
 data = pd.read_csv("../news.csv", sep=",")
-
-le = preprocessing.LabelEncoder()
-title = le.fit_transform(list(data["title"]))
-text = le.fit_transform(list(data["text"]))
-label = le.fit_transform(list(data["label"]))
-
+data = data[["title", "text", "label"]]
 predict = "label"
-
-X = list(zip(title, text))
-y = list(label)
-
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.2)
+title_list = data['title'].values.tolist()
+text_list = data['text'].values.tolist()
 
 
+# title = sklearn.feature_extraction.text.CountVectorizer(data["title"])
+#
+vectorizer = CountVectorizer()
+title_vectorized = vectorizer.fit_transform(title_list)
+text_vectorized = vectorizer.fit_transform(text_list)
+
+print(X)
+#
+# le = preprocessing.LabelEncoder()
+# label = le.fit_transform(list(data["label"]))
+# y = list(label)
+# print(y)
+
+
+
+
+#
+# le = preprocessing.LabelEncoder()
+# title = le.fit_transform(list(data["title"]))
+# text = le.fit_transform(list(data["text"]))
+# label = le.fit_transform(list(data["label"]))
+#
+# predict = "label"
+#
+# X = list(zip(title, text))
+# y = list(label)
+# #
 # x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.2)
-
-clf = svm.SVC(kernel="linear", C=2)
-clf.fit(x_train, y_train)
-
-y_predict = clf.predict(x_test)
-
-acc = metrics.accuracy_score(y_test, y_predict)
-
-print(acc)
+#
+#
+# # x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.2)
+#
+# clf = svm.SVC(kernel="linear", C=2)
+# clf.fit(x_train, y_train)
+#
+# y_predict = clf.predict(x_test)
+#
+# acc = metrics.accuracy_score(y_test, y_predict)
+#
+# print(acc)
--- linear-regression/linearregression.py
+++ linear-regression/linearregression.py
@@ -10,42 +10,47 @@ import pickle
 from matplotlib import style
 
 
-data = pd.read_csv("./linear-regression/student-mat.csv", sep=";")
+# data = pd.read_csv("./linear-regression/student-mat.csv", sep=";")
+# data = data[["G1", "G2", "G3", "studytime", "absences"]]
 
-data = data[["G1", "G2", "G3", "studytime", "absences"]]
+# predict = "G3"
 
-predict = "G3"
+
+data = pd.read_csv("./news.csv", sep=",")
+data = data[["title", "text", "label"]]
+
+predict = "label"
 
 X = np.array(data.drop(labels=[predict], axis=1))
 y = np.array(data[predict])
 
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
-# best = 0
-# for _ in range(30):
-#
-#     x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
-#
-#     linear = linear_model.LinearRegression()
-#     linear.fit(x_train, y_train)
-#     acc = linear.score(x_test, y_test)
-#     print(acc)
-#
-#     if acc > best:
-#         best = acc
-#         with open("./linear-regression/studentmodel.pickle", "wb") as f:
-#             pickle.dump(linear, f)
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.2)
+best = 0
+for _ in range(100):
 
-pickle_in = open("./linear-regression/studentmodel.pickle", "rb")
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.2)
+
+    linear = linear_model.LinearRegression()
+    linear.fit(x_train, y_train)
+    acc = linear.score(x_test, y_test)
+    print(acc)
+
+    if acc > best:
+        best = acc
+        with open("./linear-regression/fakenews.pickle", "wb") as f:
+            pickle.dump(linear, f)
+
+pickle_in = open("./linear-regression/fakenews.pickle", "rb")
 linear = pickle.load(pickle_in)
 
-predictions = linear.predict(x_test)
+# predictions = linear.predict(x_test)
 
-for x in range(len(predictions)):
-    print(predictions[x], x_test[x], y_test[x])
+# for x in range(len(predictions)):
+#     print(predictions[x], x_test[x], y_test[x])
 
-p = "absences"
-style.use("ggplot")
-pyplot.scatter(data[p], data["G3"])
-pyplot.xlabel(p)
-pyplot.ylabel("Final Grade")
-pyplot.show()
+# p = "absences"
+# style.use("ggplot")
+# pyplot.scatter(data[p], data["G3"])
+# pyplot.xlabel(p)
+# pyplot.ylabel("Final Grade")
+# pyplot.show()
